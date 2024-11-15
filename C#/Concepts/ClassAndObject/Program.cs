b1f1c41caa692b5f62ca4dcf77adbd1d0721b925
MJJoy49 <mjjoy546@gmail.com>
2024-11-15 19:13:54 +0600

    CLass&Object

---
+++ C#/Concepts/ClassAndObject/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassAndObject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object of the Teacher class
+            Teacher t1Obj = new Teacher(); 
+            
+            // Assign values to the Teacher object's properties
+            t1Obj.name = "Mr. Smith"; 
+            t1Obj.subject = "Mathematics"; 
+            t1Obj.dept = "Math Department"; 
+            t1Obj.salary = 50000;
+
+
+            //mathod call.
+            t1Obj.displayInfo();
+
+            //change dept with mathod
+            t1Obj.changeDept("CSE");
+
+            //again display mathod call
+            t1Obj.displayInfo();
+
+
+
+
+            //this way we can create more object for more teacher;
+
+
+
+
+           Console.ReadKey();   
+        }
+    }
+}
---
+++ C#/Concepts/ClassAndObject/Teacher.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassAndObject
+{
+    internal class Teacher
+    {
+
+        //properties / attribute
+        public string name;
+        public string subject;
+        public string dept;
+        public double salary;
+
+
+
+        //methods / member functions
+        public void changeDept(string newDept)
+        {
+            dept = newDept;
+        }
+
+
+
+        //methods 2 / member functions 2
+        public void displayInfo()
+        {
+            Console.WriteLine("---------------------");
+            Console.WriteLine($"Name: {this.name}");
+            Console.WriteLine($"Subject: {this.subject}");
+            Console.WriteLine($"Department: {this.dept}");
+            Console.WriteLine($"Salary: {this.salary}");
+            Console.WriteLine("---------------------");
+        }
+    }
+}
