2b3d2601df7f9b961346c002c69fab5351f835d2
MJJoy49 <mjjoy546@gmail.com>
2024-11-15 21:39:25 +0600

    CopyConstructor_Or_Reference

---
+++ C#/Concepts/CopyConstructor_Or_Reference/Doctor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace CopyConstructor_Or_Reference
+{
+    internal class Doctor
+    {
+
+        public string name;
+        public string specialty;
+        public double visitFree;
+
+
+        public Doctor(string name, string specialty, double visitFree)
+        {
+            this.name = name;
+            this.specialty = specialty;
+            this.visitFree = visitFree;
+
+        }
+
+
+        //copy constructor--------------
+        public Doctor(ref Doctor doctor)
+        {
+            this.name = doctor.name;
+            this.specialty = doctor.specialty;
+            this.visitFree = doctor.visitFree;
+
+        }
+
+
+        public void showDetails()
+        {
+            Console.WriteLine($"--------------------");
+            Console.WriteLine($"Name: {this.name}");
+            Console.WriteLine($"Specialty: {this.specialty}");
+            Console.WriteLine($"Visit Free: {this.visitFree}");
+            Console.WriteLine($"--------------------");
+
+        }
+
+
+
+
+
+
+
+    }  
+}
---
+++ C#/Concepts/CopyConstructor_Or_Reference/Program.cs
@@ -0,0 +1,41 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CopyConstructor_Or_Reference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor D1 = new Doctor("dr. Rony", "Haddi", 550);
+
+            Console.WriteLine("Befor obj refernce D1");
+            D1.showDetails();
+
+
+            // reference------------------------
+            Doctor D2 = new Doctor(ref D1);
+            Console.WriteLine("Refernce form D1 now D2 show");
+            D2.showDetails();
+
+
+            Console.WriteLine("now change value D2");
+            D2 = new Doctor("dr. Jony", "Haddi and Buk", 800);
+            D2.showDetails();
+
+
+            Console.WriteLine("");
+            Console.WriteLine("Now if we see D1 Values");
+            D1.showDetails();
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
