0f5b63073f628038f547f236368734591d8a3a15
MJJoy49 <mjjoy546@gmail.com>
2025-01-31 02:34:08 +0600

    innerDataCon

---
+++ C#/Concepts/NewL/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace NewL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtUserId = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.Done = new System.Windows.Forms.Button();
+            this.lblMsg = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(267, 153);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(95, 25);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "User ID :";
+            // 
+            // txtUserId
+            // 
+            this.txtUserId.Location = new System.Drawing.Point(382, 153);
+            this.txtUserId.Name = "txtUserId";
+            this.txtUserId.Size = new System.Drawing.Size(392, 31);
+            this.txtUserId.TabIndex = 1;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(382, 274);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(392, 31);
+            this.txtPassword.TabIndex = 3;
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(244, 280);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(118, 25);
+            this.lblEmail.TabIndex = 2;
+            this.lblEmail.Text = "Password :";
+            // 
+            // Done
+            // 
+            this.Done.Location = new System.Drawing.Point(465, 390);
+            this.Done.Name = "Done";
+            this.Done.Size = new System.Drawing.Size(182, 73);
+            this.Done.TabIndex = 4;
+            this.Done.Text = "Done";
+            this.Done.UseVisualStyleBackColor = true;
+            this.Done.Click += new System.EventHandler(this.Done_Click);
+            // 
+            // lblMsg
+            // 
+            this.lblMsg.AutoSize = true;
+            this.lblMsg.BackColor = System.Drawing.Color.Transparent;
+            this.lblMsg.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.875F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMsg.ForeColor = System.Drawing.Color.Olive;
+            this.lblMsg.Location = new System.Drawing.Point(237, 622);
+            this.lblMsg.Name = "lblMsg";
+            this.lblMsg.Size = new System.Drawing.Size(672, 61);
+            this.lblMsg.TabIndex = 5;
+            this.lblMsg.Text = "...........................................";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1162, 793);
+            this.Controls.Add(this.lblMsg);
+            this.Controls.Add(this.Done);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.txtUserId);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtUserId;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Button Done;
+        private System.Windows.Forms.Label lblMsg;
+    }
+}
+
---
+++ C#/Concepts/NewL/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace NewL
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Done_Click(object sender, EventArgs e)
+        {
+            string sqlConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\NewDatabase1.mdf;Integrated Security=True;Connect Timeout=30";
+            SqlConnection sqlConnection = new SqlConnection(sqlConnectionString);
+            
+                try
+                {
+                    sqlConnection.Open();
+                    string query = "SELECT COUNT(*) FROM Users WHERE UserId = @UserId AND Password = @Password";
+
+                    using (SqlCommand cmd = new SqlCommand(query, sqlConnection))
+                    {
+                        cmd.Parameters.AddWithValue("@UserId", txtUserId.Text.Trim());
+                        cmd.Parameters.AddWithValue("@Password", txtPassword.Text.Trim());
+
+                        int count = (int)cmd.ExecuteScalar();
+                        lblMsg.Text = count > 0 ? "Welcome to the world" : "-_-";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Database connection error: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+        }
+    }
+}
